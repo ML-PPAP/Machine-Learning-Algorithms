abf1ad25aa0116df1c3ced42bb599a1931e36df4
ML-PPAP <49780566+ML-PPAP@users.noreply.github.com>
2019-08-13 09:00:22 +0800

    Create Matplot.py

---
+++ 分类算法/Factorization Machine/Matplot.py
@@ -0,0 +1,25 @@
+#coding:UTF-8
+import matplotlib.pyplot as plt
+import numpy as np
+f = open('data')
+x1 = []
+y1 = []
+x2 = []
+y2 = []
+for line in f.readlines():
+    lines = line.strip().split("\t")
+    if lines[2].__eq__("0"):
+        x1.append(float(lines[0]))
+        y1.append(float(lines[1]))
+    else:
+        x2.append(float(lines[0]))
+        y2.append(float(lines[1]))
+f.close()
+
+plt.scatter(np.array(x1),np.array(y1),label='one')
+plt.scatter(np.array(x2),np.array(y2),label='two')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.grid(True)
+plt.show()
